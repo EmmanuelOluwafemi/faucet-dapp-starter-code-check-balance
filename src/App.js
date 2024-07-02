943de00292c6b94c89df25c4ed2a13c4aa9e9173
Emmanuel Yusuf <emaz4me@gmail.com>
2024-07-02 03:56:45 +0100

    feat: setup ui element

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar">
+        <h1 className="navbar-brand">GPT Token (GPT)</h1>
+        <div id="navbarMenu" className="navbar-menu">
+          <div>
+            <button className="button button-secondary">
+              <span className="is-link has-text-weight-bold">
+                Connect Wallet
+              </span>
+            </button>
+          </div>
+        </div>
+      </nav>
+      <section className="hero">
+        <h1 className="title">Faucet</h1>
+        <p className="subtitle">Fast and reliable. 50 GPT/day.</p>
+        <div className="box">
+          <div className="columns">
+            <input
+              className="input"
+              type="text"
+              placeholder="Enter your wallet address (0x...)"
+            />
+            <button className="button button-primary">GET TOKENS</button>
+          </div>
+          <article className="panel">
+            <p className="panel-heading">Transaction Data</p>
+            <div className="panel-block">
+              <p>transaction data</p>
+            </div>
+          </article>
+        </div>
+      </section>
     </div>
   );
 }
